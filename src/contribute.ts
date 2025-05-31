18eec4cde909e66a1755ea8328a9587aeb6bae62
Plasny <git.plasny.uq95y@slmail.me>
2025-05-31 18:02:35 +0200

    feat: create endpoint for adding new products

--- src/contribute.ts
+++ src/contribute.ts
@@ -1,26 +1,48 @@
 import Config from "./config";
 const conf = Config.getInstance();
 
-interface Contribution {
-	productId: number;
-}
+import { z } from "zod";
+const ContributionSchema = z.object({
+	lang: z.string().length(2),
+	productName: z.string().min(1),
+	productDescription: z.string().optional(),
+	packaging: z.array(z.object({
+		bin: z.enum(['glass', 'paper', 'plastic-metal', 'organic', 'non-recyclable', 'return']),
+	}))
+});
 
 const contribute = {
-	"/api/contribute/:productId": (req: Request): Response => {
-		const url = new URL(req.url);
-		const productId = parseInt(url.pathname.split("/").pop() || "");
+	"/api/contribute/:productId": {
+		POST: async (req: Request): Promise<Response> => {
+			const url = new URL(req.url);
+			const productId = parseInt(url.pathname.split("/").pop() || "");
 
-		if (isNaN(productId)) {
-			return new Response("Invalid product ID", { status: 400 });
+			if (isNaN(productId)) {
+				return new Response("Invalid product ID", { status: 400 });
+			}
+
+			let contribution
+			try {
+				contribution = ContributionSchema.parse(await req.json());
+			} catch {
+				return new Response("Bad request", { status: 400 })
+			}
+
+			let body, res
+			body = new FormData();
+			body.set("code", productId.toString())
+			body.set("user_id", conf.user)
+			body.set("password", conf.pass)
+			body.set("packaging", contribution.packaging.join(','))
+
+			res = await fetch('https://world.openproductsfacts.org/cgi/product_jqm2.pl', {
+				method: 'POST',
+				headers: { 'Content-Type': 'multipart/form-data' },
+				body: body
+			})
+
+			return new Response(null, { status: 201 });
 		}
-
-		const contribution: Contribution = {
-			productId,
-		};
-
-		return new Response(JSON.stringify(contribution), {
-			headers: { "Content-Type": "application/json" },
-		});
 	},
 };
 
