0afa00776eaa313cf96513561dbe02546ed1bf14
NikodemMarek <nikodemmarek11@gmail.com>
2025-05-31 14:24:57 +0200

    feat: remove default route and add 404 route

--- index.ts
+++ index.ts
@@ -4,10 +4,8 @@ import contribute from "./src/contribute.ts";
 Bun.serve({
 	port: 3000,
 	routes: {
-		"/api/hello": () => {
-			return new Response("Hello, World!");
-		},
 		...product,
 		...contribute,
+		"/*": (): Response => new Response("Not Found", { status: 404 }),
 	},
 });
